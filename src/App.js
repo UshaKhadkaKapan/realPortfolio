3a52efc29ed145c6553512695d244cf7739255c8
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-04-26 17:15:57 +1000

    initilizing router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Courses from "./pages/Courses";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/project" exact component={Projects} />
+        <Route path="/courses" exact component={Courses} />
+        <Route path="/contact" exact component={Contact} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
