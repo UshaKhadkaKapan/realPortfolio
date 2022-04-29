324458b6152663e94e6b67dd6477d6d5f49a7895
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-04-30 00:17:07 +1000

    moving to menu bar

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const Header = () => {
+  const [showHeader, setShowHeader] = useState(false);
+  return <div className="header"></div>;
+};
+
+export default Header;
