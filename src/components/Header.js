768bb764d9b8e5c04b6bfd4f41fd367c122416f5
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-08-18 21:02:45 +1000

    clicking events on hamburger and cross icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
 import { RiMenu3Line } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
   const [showHeader, setShowHeader] = useState(false);
   return (
     <div className="header">
-      <RiMenu3Line className=" menu-icon position-fixed top-0 end-0" />
+      {showHeader ? (
+        <AiOutlineClose
+          onClick={() => {
+            setShowHeader(!showHeader);
+          }}
+          className=" menu-icon position-fixed top-0 end-0"
+        />
+      ) : (
+        <RiMenu3Line
+          onClick={() => {
+            setShowHeader(!showHeader);
+          }}
+          className=" menu-icon position-fixed top-0 end-0"
+        />
+      )}
     </div>
   );
 };
