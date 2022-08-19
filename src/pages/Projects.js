41b832499eda1498d62e992bc4377e6f82cbcbad
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-08-19 20:08:11 +1000

    start working in project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="row flex-with-center">
+          <div className="col-md-6"></div>
+          <div className="col-md-6"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
