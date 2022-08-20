4293fe6b7bd37cf35190dd2e11198501392159f7
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-08-20 12:42:30 +1000

    contact page is complete

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,18 +6,22 @@ import "../styles/Home.css";
 const Contact = () => {
   return (
     <div>
-      {/* <Header /> */}
+      <Header />
       <div className="container contact mt-5">
         <div className="row pt-5">
           <div className="col-md-6 p-5">
             <img src="./contactus.PNG" alt="" />
           </div>
           <div className="col-md-6">
-            <div className="contact-form">
-              <input type="text" className="form-control" placeholder="Name" />
-              <input type="text" className="form-control" placeholder="Name" />
-              <textarea className="form-control" rows={3} defaultValue={""} />
-              <button className="primary-button">SUBMIT</button>
+            <div className="contact-form m-2 p-5 n-box2">
+              <h3 className="font-bold">Contact Us</h3>
+              <hr />
+              <input type="text" className="form-control " placeholder="Name" />
+              <br />
+              <input type="text" className="form-control " placeholder="Name" />
+              <br />
+              <textarea className="form-control " rows={3} defaultValue={""} />
+              <button className="primary-button mt-3 n-box2">SUBMIT</button>
             </div>
           </div>
         </div>
