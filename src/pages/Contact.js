9f041f2fc9226c83228572bf38c445bdf55b1843
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-08-20 10:31:37 +1000

    added contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Header from "../components/Header";
+import "../styles/header-footer.css";
+import "../styles/Home.css";
 
 const Contact = () => {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
+      <div className="container contact mt-5">
+        <div className="row pt-5">
+          <div className="col-md-6 p-5">
+            <img src="./contactus.PNG" alt="" />
+          </div>
+          <div className="col-md-6">
+            <div className="contact-form">
+              <input type="text" className="form-control" placeholder="Name" />
+              <input type="text" className="form-control" placeholder="Name" />
+              <textarea className="form-control" rows={3} defaultValue={""} />
+              <button className="primary-button">SUBMIT</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
